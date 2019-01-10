00b2350903745fdc7e50a0327b0414c1f1c66366
feng <1624689089@qq.com>
2019-01-10 18:49:33 +0800

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,42 @@
+import React, { Component}  from 'react';
+import PropTypes from 'prop-types';
+import logo from '../../logo.svg';
+import './Header.css'
+
+export default class PublicHeader extends Component{
+  static propTypes = {
+    record: PropTypes.any,
+    title: PropTypes.string.isRequired,
+    confirm: PropTypes.any,
+  }
+
+  state = {
+    title2: 'false',
+    seconds: 0
+  };
+
+  tick() {
+    this.setState((prveState) => ({
+      seconds: prveState.seconds + 1
+    }))
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), 1000)
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.interval)
+  }
+
+  render(){
+    return(
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div>{this.props.title}</div>
+        <div>{this.state.title2}</div>
+        <div>Seconds: {this.state.seconds}</div>
+      </header>
+    );
+  }
+}
